2b41d3836ac66922ed25e8bfb7f5fc55b8eed11b
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2022-04-29 11:01:12 -0500

    Made isEmpty and solve functions

--- Main.py
+++ Main.py
@@ -21,6 +21,31 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def emptyList(Puzzle):
+    gridList= []
+    for x in range(len(Puzzle)):
+        tupleList = []
+        for y in range(len(Puzzle[x])):
+            if Puzzle[x][y] == 0:
+                tupleList.append((x, y))
+        gridList.append(tupleList)
+    return gridList
+
+def solve(Puzzle):
+    find = isEmpty(Puzzle)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1,10):
+        if emptyList(Puzzle):
+            Puzzle[row][col] = i
+            if solve(Puzzle):
+                return True
+            Puzzle[row][col] = 0
+
+    return False
+
 # Print the Board
 def printBoard(Puzzle):
     for x in range(len(Puzzle)):
@@ -35,7 +60,6 @@ def printBoard(Puzzle):
                 print(Puzzle[x][y])
             else:
                 print(str(Puzzle[x][y]) + " ", end="")
-printBoard(board)
 
 def isEmpty(Puzzle):
     for x in range(len(Puzzle)):
@@ -43,17 +67,30 @@ def isEmpty(Puzzle):
             if Puzzle[x][y] == 0:
                 return (x, y)  # row, colum
 
-def emptyList(Puzzle):
-    gridList= []
-    for x in range(len(Puzzle)):
-        tupleList = []
-        for y in range(len(Puzzle[x])):
-            if Puzzle[x][y] == 0:
-                tupleList.append((x, y))
-        gridList.append(tupleList)
-    return gridList
 
-# def solve(Puzzle):
 
-#     find = isEmpty(Puzzle)
-    
+
+printBoard(board)
+solve(board)
+print("*******************************")
+printBoard(board)
+
+
+ # Checks raw and column
+# def validNum(Puzzle, num, pos):
+#     for x in range(len(Puzzle[0])):
+#         if Puzzle[pos[0]][x] == num and pos[1] != x:
+#             return False
+#     for y in range(len(Puzzle)):
+#         if Puzzle[y][pos[1]] == num and pos[0] != y:
+#             return False
+#     # Check box
+#     box_x = pos[1] // 3
+#     box_y = pos[0] // 3
+#
+#     for i in range(box_y*3, box_y*3 + 3):
+#         for j in range(box_x * 3, box_x*3 + 3):
+#             if Puzzle[i][j] == num and (i,j) != pos:
+#                 return False
+#
+#     return True
