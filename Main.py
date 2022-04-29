0fdb5ac1771bafc0d4e6f1ad2848bfdcfeb44ac1
vosien <davidjiang.mr@gmail.com>
2022-04-29 11:53:15 -0500

    Update Main.py
    Organized file and resolved functions that didn't run properly. Added comments for clarity.

--- Main.py
+++ Main.py
@@ -10,19 +10,37 @@ from matplotlib.pyplot import grid
 
 
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
-def emptyList(Puzzle):
-    gridList= []
+
+# print the board
+def printBoard(Puzzle):
+    for x in range(len(Puzzle)):
+        if x % 3 == 0 and x != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for y in range(len(Puzzle[x])):
+            if y % 3 == 0 and y != 0:
+                print(" | ", end="")
+
+            if y == 8:
+                print(Puzzle[x][y])
+            else:
+                print(str(Puzzle[x][y]) + " ", end="")\
+
+
+# create a nested list with each list representing a row and each value being the coordinate of an empty cell
+def emptyMatrix(Puzzle):
+    gridList = []
     for x in range(len(Puzzle)):
         tupleList = []
         for y in range(len(Puzzle[x])):
@@ -31,64 +49,43 @@ def emptyList(Puzzle):
         gridList.append(tupleList)
     return gridList
 
-def solve(Puzzle):
-    find = isEmpty(Puzzle)
-    if not find:
-        return True
-    else:
-        row, col = find
-    for i in range(1,10):
-        if emptyList(Puzzle):
-            Puzzle[row][col] = i
-            if solve(Puzzle):
-                return True
-            Puzzle[row][col] = 0
 
-    return False
-
-# Print the Board
-def printBoard(Puzzle):
-    for x in range(len(Puzzle)):
-        if x % 3 == 0 and x != 0:
-            print("- - - - - - - - - - - - - ")
-
-        for y in range(len(Puzzle[0])):
-            if y % 3 == 0 and y != 0:
-                print(" | ", end="")
-
-            if y == 8:
-                print(Puzzle[x][y])
-            else:
-                print(str(Puzzle[x][y]) + " ", end="")
-
-def isEmpty(Puzzle):
-    for x in range(len(Puzzle)):
-        for y in range(len(Puzzle[x])):
-            if Puzzle[x][y] == 0:
-                return (x, y)  # row, colum
+# def isEmpty(Puzzle):
+#     for x in range(len(Puzzle)):
+#         for y in range(len(Puzzle[x])):
+#             if Puzzle[x][y] == 0:
+#                 return True  # row, column
 
 
+# def solve(Puzzle):
+#     empty = isEmpty(Puzzle)
+#     if not empty:
+#         return True
+#     else:
+#         row, col = empty
+#     for i in range(1,10):
+#         if emptyMatrix(Puzzle):
+#             Puzzle[row][col] = i
+#             if solve(Puzzle):
+#                 return True
+#             Puzzle[row][col] = 0
+#
+#     return False
 
 
-printBoard(board)
-solve(board)
-print("*******************************")
-printBoard(board)
-
-
- # Checks raw and column
+# checks validity of number in cell by comparing across row, column, and box
 # def validNum(Puzzle, num, pos):
-#     for x in range(len(Puzzle[0])):
+#     for x in range(len(Puzzle[0])):  # checks validity across all cells in row
 #         if Puzzle[pos[0]][x] == num and pos[1] != x:
 #             return False
-#     for y in range(len(Puzzle)):
+#     for y in range(len(Puzzle)):  #checks validity across all cells in column
 #         if Puzzle[y][pos[1]] == num and pos[0] != y:
 #             return False
-#     # Check box
-#     box_x = pos[1] // 3
-#     box_y = pos[0] // 3
+
+#     box_x = pos[1] // 3   # determines which box horizontally
+#     box_y = pos[0] // 3   # determines which box vertically
 #
-#     for i in range(box_y*3, box_y*3 + 3):
+#     for i in range(box_y*3, box_y*3 + 3):  # checks validity across all cells in box
 #         for j in range(box_x * 3, box_x*3 + 3):
 #             if Puzzle[i][j] == num and (i,j) != pos:
 #                 return False
